845a018e7b43488976615069d07d2360abc7950d
Rezalt0 <https://github.com/Rezalt0>
2026-01-22 01:35:12 +0300

    add ext for fetch

---
+++ chapter1/fetch/ext1.9/fetch.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const httpPrefix = "http://"
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = fmt.Sprintf("%s%s", httpPrefix, url)
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("status code: %d. for url: %s\n", resp.StatusCode, url)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: read %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
